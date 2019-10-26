d0aefa12d5dd099c07e3e40a57504baccf196a7e
albany09 <wlsdyd82@naver.com>
2019-10-26 17:35:54 +0900

    완료

---
+++ app/src/main/java/com/example/bookstore/main/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.bookstore.main;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bookstore.R;
+import com.example.bookstore.base.BaseActivity;
+import com.example.bookstore.model.Movie;
+
+import java.util.List;
+
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity<MainContract.View, MainContract.Presenter>
+        implements MainContract.View
+{
+
+    MainAdapter adapter;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    List<Movie> movies;
+
+    @BindView(R.id.progress)
+    ProgressBar progressBar;
+
+    @Override
+    protected MainContract.Presenter setPresenter() {
+        return new MainPresenter();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+//        btnFetchItems.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //TODO 클릭시 Presenter로 다운스트림 발생
+//                mPresenter.fetchMovies(1);
+//            }
+//        });
+    }
+
+    private void initRecyclerView(){
+        adapter = new MainAdapter(movies, mPresenter);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void fetchMoviesDone(List<Movie> list) {
+        progressBar.setVisibility(View.GONE);
+        movies.addAll(list);
+        Log.d("MainActivity", "list size " + list.size());
+        adapter.notifyDataSetChanged();
+    }
+}
